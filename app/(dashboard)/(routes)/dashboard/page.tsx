65e1060a52a9da7efec9cd05bc5c8b7ef63152ad
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2023-08-01 19:15:07 +0530

    some new chnages

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    <Button variant="danger">Click me </Button>
+  )
+}
